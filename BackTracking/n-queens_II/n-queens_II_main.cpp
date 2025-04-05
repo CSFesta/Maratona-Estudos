1849d0cf6ce264b139e31eb0ec48665d6ecd171c
CSFesta <cauafesta@hotmail.com>
2025-04-05 11:16:54 -0300

    add n_queens_II

---
+++ BackTracking/n-queens_II/n-queens_II_main.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int ans = 0;
+set<int> posD;
+set<int> negD;
+vector<bool> cols;
+
+void backtrack(vector<vector<char>>&board, int row){
+    if(row == board.size()){
+        ans++;
+        return;
+    }
+    
+    for(int col = 0; col < board.size(); col++){
+        if(cols[col] || posD.count(row + col) || negD.count(row - col)){
+            continue;
+        }
+        cols[col] = true;
+        posD.insert(row + col);
+        negD.insert(row - col);
+        board[row][col] = 'Q';
+
+        backtrack(board, row + 1);
+
+        cols[col] = false;
+        posD.erase(row + col);
+        negD.erase(row - col);
+        board[row][col] = '.';
+    }
+}
+
+int main(){
+    int n = 4;
+    vector<vector<char>> board (n, vector<char>(n, '.'));
+    cols.resize(n);    
+    backtrack(board, 0);
+    cout << ans << "\n";
+    return 0;
+}
